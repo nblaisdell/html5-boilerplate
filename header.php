eb0ca48328b6985877916ee224a8ee771d3db94a
Nathan Blaisdell <nblaisdell@gmail.com>
2013-04-02 22:23:00 -0500

    more customization

--- header.php
+++ header.php
@@ -10,4 +10,15 @@
 <link rel="stylesheet" href="css/normalize.css">
 <link rel="stylesheet" href="css/main.css">
 <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+<!-- Firefox specific search, don't forget to add the title of the site: -->
+<link rel="search" type="application/opensearchdescription+xml" href="/opensearch.xml" title="        ">
 </head><body>
+
+<header>
+    header
+</header>
+
+    <!--[if lt IE 7]>
+    <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+    <![endif]-->
+    
