532daedf58c7e808c5489de9b256566834ce2b07
Nathan Blaisdell <nblaisdell@5thcinema.com>
2013-11-11 20:00:14 -0500

    commit

--- incl/header.php
+++ incl/header.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html><head>
+<!DOCTYPE html><html lang="en"><head>
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 <title></title>
@@ -8,14 +8,57 @@
 <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
 
 <link rel="stylesheet" href="css/normalize.css">
-<link rel="stylesheet" href="css/main.css">
+<link rel="stylesheet" href="css/style.css">
 <script src="js/vendor/modernizr-2.6.2.min.js"></script>
 <!-- Firefox specific search, don't forget to add the title of the site: -->
 <link rel="search" type="application/opensearchdescription+xml" href="/opensearch.xml" title="        ">
 </head><body>
 
-<header>
-    header
-</header>
+    <header class="comp-header">
+        <ul>
+            <li>
+                this
+            </li>
+            <li>
+                is
+            </li>
+            <li>
+                the
+            </li>
+            <li>
+                menu
+            </li>
+            <li>
+                stuff
+            </li>
+            <li>
+                yo
+            </li>
+        </ul>
+    </header>
+
+    <div class="mobile-header">
+        <select>
+            <option>
+                this
+            </option>
+            <option>
+                is
+            </option>
+            <option>
+                the
+            </option>
+            <option>
+                menu
+            </option>
+            <option>
+                stuff
+            </option>
+            <option>
+                yo
+            </option>
+        </select>
+    </div>
+
 
 <!--[if lt IE 7]><p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p><![endif]-->
