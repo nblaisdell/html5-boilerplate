01f8e65052879dc715fb35e19ab751e23e6ad4b6
Nathan Blaisdell <nblaisdell@5thcinema.com>
2013-11-14 11:18:02 -0500

    Revert 5b2e287..532daed
    This rolls back to commit 5b2e28773fec07c1095b2ccac737a72806286a70.

--- index.php
+++ index.php
@@ -2,12 +2,5 @@
 
     <!-- Add your site or application content here -->
     <p>Hello world! This is HTML5 Boilerplate.</p>
-    <h1>heading</h1>
-    <h2>heading</h2>
-    <h3>heading</h3>
-    <h4>heading</h4>
-    <h5>heading</h5>
-    <h6>heading</h6>
-
 
 <?php include("incl/footer.php"); ?>
